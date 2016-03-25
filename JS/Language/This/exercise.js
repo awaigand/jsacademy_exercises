d00b2cb5c0c9cd00daf08c45bb45492f114a1153
Andreas Waigand <awaigand@gmx.net>
2016-03-25 02:58:59 +0100

    Update exercise.js

--- JS/Language/This/exercise.js
+++ JS/Language/This/exercise.js
@@ -24,18 +24,20 @@ fooObj.log4 = newLog2;
 
 //EDIT-START
 
-console.log(foo === "foo");
-console.log(this.foo === "foo");
-console.log(fooObj.log1().foo === "baz");
-console.log(fooObj.log2().foo === "foo");
-console.log(log1().foo === "foo");
-console.log(log1.call(fooObj).foo === "baz");
-console.log(new log1().foo === "bar");
-console.log(newLog1().foo === "bad");
-console.log(newLog2().foo === "foo");
-console.log(fooObj.log3().foo === "bad");
-console.log(fooObj.log4().foo === "foo");
-console.log((fooObj.log1)().foo === "baz");
-console.log((1,fooObj.log1)().foo === "foo");
+var makeAllTrue = [
+	foo === "foo",
+	this.foo === "foo",
+	fooObj.log1().foo === "baz",
+	fooObj.log2().foo === "foo",
+	log1().foo === "foo",
+	log1.call(fooObj).foo === "baz",
+	new log1().foo === "bar",
+	newLog1().foo === "bad",
+	newLog2().foo === "foo",
+	fooObj.log3().foo === "bad",
+	fooObj.log4().foo === "foo",
+	(fooObj.log1)().foo === "baz",
+	(1,fooObj.log1)().foo === "foo"
+];
 
 //EDIT-END
