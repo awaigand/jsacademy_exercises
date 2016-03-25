adee6bbd8395d595c8d3e09243df305815c31e40
Andreas Waigand <awaigand@gmx.net>
2016-03-26 00:10:10 +0100

    Update test.js

--- JS/Language/LexicalScope/test.js
+++ JS/Language/LexicalScope/test.js
@@ -1,9 +1,13 @@
-describe("this-Binding-Exercise", function() {
-    it("Think of the current scope!", function() {
+describe("Lexical-Scope-Exercise", function() {
+    it("Think of closure scope, especially for the third one!", function() {
       expect(makeAllTrue[0]).to.be.true;
     });
 
-    it("What does 'this' default to?", function() {
+    it("Basenumber and the iterator variable were changed. Think of the closure scopes!", function() {
       expect(makeAllTrue[1]).to.be.true;
+    });
+    
+    it("For the lexical scope, the only thing that matters is where the function was defined.", function() {
+      expect(makeAllTrue[2]).to.be.true;
     });
 });
